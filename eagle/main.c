a8d51306cf317cbbc837e970ca89fffd4da3f978
Elliot Chance <elliotchance@gmail.com>
2012-11-28 19:29:01 +1100

    Initial Commit

---
+++ eagle/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  eagle
+//
+//  Created by Elliot Chance on 28/11/12.
+//  Copyright (c) 2012 Elliot Chance. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
+
