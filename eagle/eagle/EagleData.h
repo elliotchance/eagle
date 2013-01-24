3bd448b0fa4a005b367fb61086e4504e442eb3f8
Elliot Chance <elliotchance@gmail.com>
2013-01-24 16:03:40 +1100

    [#36] Minor

--- eagle/eagle/EagleData.h
+++ eagle/eagle/EagleData.h
@@ -16,6 +16,7 @@ typedef enum {
  Allocate an integer and return the pointer.
  
  @param [in] value Value to set into newly allocated space.
+ @return New allocation containing the integer.
  */
 int* EagleData_Int(int value);
 
