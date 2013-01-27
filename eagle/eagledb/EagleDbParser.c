54215536686a9450427d552ef87a5580d96a3ca0
Elliot Chance <elliotchance@gmail.com>
2013-01-27 17:27:21 +1100

    [#33] Move the parser functions to EagleDbParser

--- eagle/eagledb/EagleDbParser.c
+++ eagle/eagledb/EagleDbParser.c
@@ -1,5 +1,11 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "Eagle.h"
 #include "EagleDbParser.h"
+#include "EagleDbSqlStatementType.h"
+#include "EagleMemory.h"
+#include "EagleData.h"
 
 char **yyerrors = NULL;
 int yyerrors_length = 0;
@@ -10,3 +16,135 @@ void **yylist = NULL;
 int yylist_length = 0;
 void **yyreturn = NULL;
 int yyreturn_length = 0;
+
+EagleDbSqlStatementType yystatementtype = EagleDbSqlStatementTypeNone;
+
+/**
+ Push the error onto the stack.
+ */
+char* yyerrors_push(void *ptr)
+{
+    /* we have reached the maximum amount of errors, in this case we just don't store any more errors */
+    if(yyerrors_length >= MAX_YYERRORS) {
+        free(ptr);
+        return NULL;
+    }
+    return yyerrors[yyerrors_length++] = ptr;
+}
+
+/**
+ Returns the most recent error message.
+ */
+char* yyerrors_last()
+{
+    return yyerrors[yyerrors_length - 1];
+}
+
+/**
+ Anything that is allocated during the parsing must call yyobj_push(), if the parsing fails this stack will do the cleanup.
+ */
+void* yyobj_push(void *ptr)
+{
+    if(yyobj_length >= MAX_YYOBJ) {
+        char *msg = (char*) malloc(64);
+        sprintf(msg, "Cannot parse SQL. Maximum depth of %d exceeded.", MAX_YYOBJ);
+        yyerrors_push(msg);
+        return NULL;
+    }
+    return yyobj[yyobj_length++] = ptr;
+}
+
+void* yylist_push(void *ptr)
+{
+    if(yylist_length >= MAX_YYLIST) {
+        char *msg = (char*) malloc(64);
+        sprintf(msg, "Cannot parse SQL. Maximum list size of %d exceeded.", MAX_YYLIST);
+        yyerrors_push(msg);
+        return NULL;
+    }
+    return yylist[yylist_length++] = ptr;
+}
+
+void* yylist_new()
+{
+    yylist = (void**) EagleMemory_MultiAllocate("yylist_new.1", sizeof(void*), MAX_YYLIST);
+    yylist_length = 0;
+    return yylist;
+}
+
+/**
+ Push the return value onto the stack.
+ */
+void* yyreturn_push(void *ptr)
+{
+    if(yyreturn_length >= MAX_YYRETURN) {
+        char *msg = (char*) malloc(64);
+        sprintf(msg, "Cannot parse SQL. Maximum return depth of %d exceeded.", MAX_YYRETURN);
+        yyerrors_push(msg);
+        return NULL;
+    }
+    return yyreturn[yyreturn_length++] = ptr;
+}
+
+/**
+ Return the last yyreturn and decrement back the stack.
+ */
+void* yyreturn_pop()
+{
+    return yyreturn[--yyreturn_length];
+}
+
+/**
+ Return the most recent yyreturn.
+ */
+void* yyreturn_current()
+{
+    return yyreturn[yyreturn_length - 1];
+}
+
+EagleDataType yy_data_type;
+
+int yyerror(char *s)
+{
+    yyerrors_push(strdup(s));
+    return 0;
+}
+
+void yylex_init()
+{
+    yyobj = (void**) EagleMemory_MultiAllocate("yylex_init.1", sizeof(void*), MAX_YYOBJ);
+    yyobj_length = 0;
+    
+    yyerrors = (char**) EagleMemory_MultiAllocate("yylex_init.2", sizeof(char*), MAX_YYERRORS);
+    yyerrors_length = 0;
+    
+    yyreturn = (void**) EagleMemory_MultiAllocate("yylex_init.3", sizeof(void*), MAX_YYRETURN);
+    yyreturn_length = 0;
+}
+
+void yylex_free()
+{
+    int i;
+    
+    /* yyobj */
+    if(yyerrors_length > 0) {
+        for(i = 0; i < yyobj_length; ++i) {
+            EagleMemory_Free(yyobj[i]);
+        }
+    }
+    EagleMemory_Free(yyobj);
+    yyobj = NULL;
+    
+    /* yyreturn */
+    EagleMemory_Free(yyreturn);
+    yyreturn = NULL;
+    
+    /* yyerrors */
+    for(i = 0; i < yyerrors_length; ++i) {
+        EagleMemory_Free(yyerrors[i]);
+    }
+    EagleMemory_Free(yyerrors);
+    yyerrors = NULL;
+    
+    yylex_destroy();
+}
--- eagle/eagledb/EagleDbParser.h
+++ eagle/eagledb/EagleDbParser.h
@@ -1,6 +1,8 @@
 #ifndef eagle_EagleDbParser_h
 #define eagle_EagleDbParser_h
 
+#include "EagleData.h"
+
 #define MAX_YYOBJ 256
 #define MAX_YYERRORS 100
 #define MAX_YYRETURN 256
@@ -12,21 +14,32 @@
 int yy_scan_string(const char *str);
 
 /**
+ This can be used to get the most recent yytext token. This is not a data duplication of the token so you must copy it
+ out if you intended to keep it. Since you cannot access the yytext direct from here?
+ */
+extern char *yytext_last;
+
+/**
  Internal prototype provided by flex and bison.
  */
-int yyparse();
+int yyparse(void);
+
+/**
+ Used by "data_type"
+ */
+extern EagleDataType yy_data_type;
 
 int yyerror(char *s);
 
-int yylex();
+int yylex(void);
 
-int yylex_destroy();
+int yylex_destroy(void);
 
-char* yyerrors_last();
+char* yyerrors_last(void);
 
-void yylex_init();
+void yylex_init(void);
 
-void yylex_free();
+void yylex_free(void);
 
 char* yyerrors_push(void *ptr);
 
@@ -34,7 +47,7 @@ void* yyobj_push(void *ptr);
 
 void* yylist_push(void *ptr);
 
-void* yylist_new();
+void* yylist_new(void);
 
 void* yyreturn_push(void *ptr);
 
@@ -64,5 +77,9 @@ extern int yylist_length;
 extern void **yyreturn;
 
 extern int yyreturn_length;
+
+void* yyreturn_pop(void);
+
+void* yyreturn_current(void);
 
 #endif
