745a2356e76b5a3f5c30d7ddbe222ea5afdc3b61
Elliot Chance <elliotchance@gmail.com>
2013-02-23 00:52:34 +1100

    [#60] CLI test suite is not working with some basic commands

--- bin/cli.sh
+++ bin/cli.sh
@@ -1,25 +1,39 @@
 #!/usr/bin/expect
 
 set timeout 2
+set sql_i 1
+
+set sql(1)   "create table mytable ( col1 int )"
+set sql_r(1) "Table 'mytable' created."
+set sql(2)   "select col1 from mytable"
+set sql_r(2) {.*0 records.*}
+set sql(3)   "insert into mytable (col1) values (123)"
+set sql_r(3) {.*}
+set sql(4)   "select col1 from mytable"
+set sql_r(4) {.*1 record.*}
 
 spawn "/Users/elliot/Library/Developer/Xcode/DerivedData/eagle-bvzxfkxjojdzztarraazawlgiejc/Build/Products/Debug/eagle"
 
-proc run_sql {sql expected} {
-	puts $sql
-	expect "eagle> " {
-		send "$sql;\r"
-		expect {
-			-re $expected {
-				# do nothing
+proc run_sql {} {
+	global sql_i sql sql_r
+	
+	send "$sql($sql_i);\r"
+	expect {
+		-re $sql_r($sql_i) {
+			# next command
+			incr sql_i
+			if {[info exists sql($sql_i)]} {
+				run_sql
 			}
-			timeout {
-				exit 1
-			}
+		}
+		timeout {
+			exit 1
 		}
 	}
 }
 
-run_sql "create table mytable ( col1 int )" "Table 'mytable' created."
-run_sql "select * from mytable" {.*: .USERERROR. mytable}
+run_sql
+puts "\n"
+exit
 
 interact
